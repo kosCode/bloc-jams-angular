b4b72d71678df6fed8593393d17ed2ac6b2c74f5
Kevin Kosiorek <Kevin Kosiorek>
2016-10-10 10:42:44 -0400

    assign-RoutingStates

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,26 +1,27 @@
-angular.module('blocJams', []);
-
 (function() {
     function config($stateProvider, $locationProvider) {
-      $locationProvider
-          .html5mode({
-              enabled: true,
-              requiredBase: false
-          });
+       $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 
-      $stateProvider
-          .state('landing', {
-              url: '/',
-              templateUrl: '/templates/landing.html'
-          })
-          .state('album', {
-              url: '/album',
-              templateUrl: '/templates/album.html'
-          });
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            templateUrl: '/templates/landing.html'
+        })
+        .state('album', {
+            url: '/album',
+            templateUrl: '/templates/album.html'
+        })
+        .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
+        });
     }
 
     angular
-      .module('blocJams',['ui.router'])
-      .config(config);
-
-  })();
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
