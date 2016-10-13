63d4ca8e90fc4681aa9fa4295ee9554db1d204d8
Kevin Kosiorek <Kevin Kosiorek>
2016-10-12 20:59:48 -0400

    assign-7-Services2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,7 +3,17 @@
         var SongPlayer = {};
 
         var currentSong = null;
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
+
+        /**
+         * @function setSong
+         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         */
 
         var setSong = function (song) {
             if (currentBuzzObject) {
@@ -19,28 +29,49 @@
             currentSong = song;
         };
 
+/**
+ * @function playSong
+ * @desc Uses the Buzz library to play the sound file, and flips the CurrentSong playing flag to true
+ * @param {Object} song
+ */
+
+        playSong = function () {
+            currentBuzzObject.play();
+            currentSong.playing = true;
+        }
+
+/**
+ * @function SongPlayer.play 
+ * @desc This will play a song if it is paused, or set a new current song and play the nwly created song.   
+ * @param {Object} song
+ */
+
         SongPlayer.play = function (song) {
 
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong();
 
 
             } else if (currentSong === song) {
 
-                if (currentBuzzObject.ispaused()) {
-                    currentBuzzObject.play();
-                    song.playing = true;
-                    
+                if (currentBuzzObject.isPaused()) {
+                    playSong();
+
                 }
             }
         };
 
+/**
+ * @function SongPlayer.pause 
+ * @desc This will pause the curently playing song.  
+ * @param {Object} song
+ */
+
         SongPlayer.pause = function (song) {
             currentBuzzObject.pause();
             song.playing = false;
-            
+
         }
 
         return SongPlayer;
